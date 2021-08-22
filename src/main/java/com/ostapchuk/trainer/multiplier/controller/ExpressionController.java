d4646084a22059f927a6bfb1722fddcb1e4c6d66
xfase <xfaseex1@gmail.com>
2021-08-22 13:46:35 +0300

    feat: add all the project

--- src/main/java/com/ostapchuk/trainer/multiplier/controller/ExpressionController.java
+++ src/main/java/com/ostapchuk/trainer/multiplier/controller/ExpressionController.java
@@ -11,36 +11,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.security.RolesAllowed;
-
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000")
 @Slf4j
-public class ResultController {
+public class ExpressionController {
 
     private final MultiplierService multiplierService;
 
-    @RolesAllowed({ "ADMIN", "USER" })
     @GetMapping("/easy")
     public Expression generateEasyExpression() {
         return multiplierService.generateEasyExpression();
     }
 
-    @RolesAllowed({ "ADMIN" })
     @GetMapping("/medium")
     public Expression generateMediumExpression() {
         return multiplierService.generateMediumExpression();
     }
 
-    @RolesAllowed({ "ADMIN" })
     @GetMapping("/hard")
     public Expression generateHardExpression() {
         return multiplierService.generateHardExpression();
     }
 
-    @RolesAllowed({ "ADMIN", "USER" })
     @PostMapping()
     public boolean checkAttempt(@RequestBody Expression expression) {
         boolean result = multiplierService.checkAttempt(expression);
