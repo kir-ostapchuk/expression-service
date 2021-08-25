c3e690d588b39c1e5c6eb27ada36a8f63cb49db1
xfase <xfaseex1@gmail.com>
2021-08-25 12:39:48 +0300

    refactor: remove frontend

--- frontend/src/service/APIService.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "http://localhost:8090/",
-    // baseURL: "http://multiplicationtrainer-env.eba-8serprck.us-east-2.elasticbeanstalk.com/",
-    responseType: "json"
-});
